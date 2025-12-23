7a118069ff190693314a8822fa35b7ec64ab9e39
Tim Hoff <timothy.w.hoff@gmail.com>
2025-12-23 11:25:13 -0500

    Update to latest template and add README

--- src/platform.ts
+++ src/platform.ts
@@ -1,7 +1,7 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { DakotaMotionSensorAccessory } from './platformAccessory';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
+import { DakotaMotionSensorAccessory } from './platformAccessory.js';
 
 /**
  * HomebridgePlatform
